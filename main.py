ea2b68eca4f18750d2118117749d493e12a96765
Sal1r <107485701+Sal1r@users.noreply.github.com>
2022-06-29 12:27:37 +0300

    Little improvments in code and filled database

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sqlite3 as sq
 
-text = input()
+text = input("Enter any text")
 cur = sq.connect('db.db').cursor()
 keys = cur.execute("SELECT * FROM keys").fetchall()
 
 if text == "":
-	print("Please try again and input any chars")
+	print("\nPlease try again and enter any text")
 	exit()
 for k in keys:
 	text.replace(k[0], k[1])
